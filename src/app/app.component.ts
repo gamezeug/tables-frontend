5995b2554261103076fc7d56efd7d0e97f73a810
Artun Subasi <asubasi@gmail.com>
2019-10-30 10:55:43 +0100

    Trying out the new backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gamezeug-tables';
+
+  constructor(private http: HttpClient) { }
+
+  onClickMe() {
+    this.getTest().subscribe((data: string) => this.title = data);
+  }
+
+  getTest() {
+    return this.http.get('http://localhost:8081/test');
+  }
+
 }
